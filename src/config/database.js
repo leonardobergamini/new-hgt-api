e9217dfb03048ad1268065a3a8cb4799d8847249
Leonardo Bergamini <leonardobergamini@MacBook-Air-de-Leonardo.local>
2020-03-06 00:04:13 -0300

    initial commit

---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'ec2-35-172-85-250.compute-1.amazonaws.com',
+  database: 'd2t1uq6sr7qgbr',
+  username: 'bardogwnyhbqet',
+  password: '2f61b6b4e2944f2353049873d1c3eea41d1ab92bafc68b815efe1dceb2d400a3',
+  extras: {
+    ssl: true,
+  },
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
